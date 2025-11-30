332ca97367e22e7277e6b0cf2fd0111ca150276c
Asiffisaaa <fisokuhlendimande96@gmail.com>
2025-12-01 01:45:13 +0200

    updated api.jsx

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "http://localhost:5000/api", // ✔ correct
 });
-
 
 export default API;
